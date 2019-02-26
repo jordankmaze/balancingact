27a1b39dde87fa580e3e9b80d07875671efc8082
jordankmaze <jordankmaze@gmail.com>
2019-02-25 21:58:43 -0800

    add game over functionality

--- clickme-master/static/script.js
+++ clickme-master/static/script.js
@@ -1,7 +1,8 @@
 (function() {
     var container1 = document.getElementById("container1");
     var container2 = document.getElementById("container2");
-    var button = document.getElementById("button");
+    var button1 = document.getElementById("button1");
+    var button2 = document.getElementById("button2");
     var scoreboard = document.getElementById("scoreboard");
     var levelboard = document.getElementById("levelboard");
     var banner = document.getElementById("banner");
@@ -22,6 +23,8 @@
     let medium = 1000;
     let fast = 500;
     let insane = 250;
+    let btnSwitcher = 0;
+    let lastRandomBtn = 0;
 
     //game play
     //level
@@ -34,34 +37,64 @@
     	failure restarts score
     	continue to next level with faster times
     */
+    
+
     startGame = (speed) => {
         setInterval(() => {
             var current = document.getElementsByClassName("btn");
-            let btnSwitcher = Math.round(Math.random())
-            current[btnSwitcher].classList.toggle("active");
-            this.className += " active";
+
+            while (btnSwitcher === lastRandomBtn) {
+                btnSwitcher = Math.round(Math.random());
+            }
+            lastRandomBtn = btnSwitcher;
+            let isButtonLit = current[btnSwitcher].classList.toggle("active");
+            enableBtn(isButtonLit);
         }, speed);
+    };
+
+    enableBtn = (isButtonLit) => {
+        //let isButtonLit = current[btnSwitcher].classList.toggle("active");
+        //console.log(isButtonLit);
+        return isButtonLit;
     }
 
+    disableBtn = () => {
+        //button1.disabled = true;
+        //button2.disabled = true;
+    }
 
-    button.addEventListener("click", () => {
-        addToScore();
-        button.classList.toggle("active");
+    button1.addEventListener("click", () => {
+        if (enableBtn() === true) {
+            addToScore();
+            button1.classList.toggle("active");
+        } else {
+            gameOver();
+            startGame(slow);
+        }
     });
 
-    // button2.addEventListener("click", () => {
-    //     addToScore();
-    //     button2.classList.toggle("active");
-    // });
+    button2.addEventListener("click", () => {
+        if (enableBtn() === true) {
+            addToScore();
+            button2.classList.toggle("active");
+        } else {
+            gameOver();
+            startGame(slow);
+        }
+    });
 
-    // initial banner animation
-    animateBanner(banner);
+    gameOver = () => {
+        level = 1;
+        score = 0;
+        button1.innerHTML = `GAME`;
+        button2.innerHTML = `OVER`;
+        button1.classList.toggle("game-over");
+        button2.classList.toggle("game-over");
+    }
 
     addToScore = () => {
         score++;
         scoreboard.innerHTML = `Score: ${score}`;
-
-        //animateScoreboard(scoreboard);
 
         // level change
         if ((score === 10) || (score === 20) || (score === 30)) {
@@ -123,12 +156,13 @@
         }, 10);
     }
 
+    animateBanner(banner);
+
     function animateBanner(banner) {
         centerBanner(banner);
         if (score === 0) centerContainer(container1);
         if (score === 0) centerContainer(container2);
         fadeOut(banner);
-        //fadeIn(banner);
         startGame(slow);
         
     }
